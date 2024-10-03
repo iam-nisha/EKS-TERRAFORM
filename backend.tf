9fc1b54f7836c03b045049168addcea29de0ac64
iam-nisha <namdeonishajbp@gmail.com>
2024-10-03 14:21:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "nisha12ekstf" # Replace with your actual S3 bucket name
+    bucket = "nisha12eks" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
